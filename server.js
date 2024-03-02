3b9c30161bb71fb6a7364c90f53a216baf7c5b6b
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-02 17:01:58 +0700

    finish

--- server.js
+++ server.js
@@ -870,6 +870,32 @@ server.post("/user-written-blogs-count", verifyJWT, (req, res) => {
     })
 })
 
+server.post("/delete-blog", verifyJWT, (req, res) => {
+
+    let user_id = req.user
+    let { blog_id } = req.body
+
+    Blog.findOneAndDelete({ blog_id })
+    .then(blog => {
+
+        Notification.deleteMany({ blog: blog._id })
+        .then(data => console.log("Notification deleted"))
+
+        Comment.deleteMany({ blog_id: blog._id })
+        .then(data => console.log("Comment deleted"))
+
+        User.findOneAndUpdate({ _id: user_id }, { $pull: { blog: blog._id }, $inc: { "account_info.total_posts": -1 } })
+        .then(user => console.log("blog deleted"))
+
+        return res.status(200).json({ status: "Done" })
+
+    })
+    .catch(err => {
+        return res.status(500).json({ error: err.message })
+    })
+
+})
+
 server.listen(PORT, () => {
     console.log('listening on port --> ' + PORT)
 })
