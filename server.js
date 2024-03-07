db8423f9dd4be64839bd6e8c56b87775fe7635a7
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-07 11:21:32 +0700

    deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { MongoClient } = require("mongodb")
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
@@ -17,6 +18,9 @@ const Comment = require("./Schema/Comment")
 const server = express()
 const PORT = process.env.PORT || 8000
 
+const uri = process.env.DB_LOCATION
+const client = new MongoClient(uri)
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccountKey)
 })
@@ -27,16 +31,16 @@ let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
 server.use(express.json())
 server.use(cors())
 
-const connectDB = async () => {
-    try {
-        const conn = mongoose.connect(process.env.DB_LOCATION, {
-            autoIndex: true
-        })
-        console.log("Connected to mongoose");
-    } catch (error) {
-        console.log(error);
-    }
-}
+// const connectDB = async () => {
+//     try {
+//         const conn = mongoose.connect(process.env.DB_LOCATION, {
+//             autoIndex: true
+//         })
+//         console.log("Connected to mongoose");
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
 // Setting Up AWS S3 Bucket
 const s3 = new AWS.S3({
@@ -907,7 +911,12 @@ server.get("/", (req, res) => {
     res.json({ message: "Pen n Pixel API"})
 })
 
-connectDB().then(() => {
+client.connect(err => {
+    if (err) {
+        console.log(err);
+        return false
+    }
+    
     server.listen(PORT, () => {
         console.log('listening on port --> ' + PORT)
     })
